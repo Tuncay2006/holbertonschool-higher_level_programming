b16eb0bf618592bc981603a95b29bf8fc11d0d97
Tuncay2006 <zenixthedark@gmail.com>
2025-06-17 15:00:44 +0000

    thrsrea

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 # htt
-if __name__ == "__main__":
-    from add_0 import add
-    a=1
-    b=2
-    print("{:d} + {:d} = {:d}" .format (a,b,add(a,b)))
+def add(a, b):
+    return a + b
+
--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 # add_0.py dosyasından add fonksiyonunu getiriyoruz
-from add_0 import add
+from add_0 import add  # <--- add_0 burada sadece 1 kez geçiyor
 
-# a ve b değişkenlerini farklı satırlarda tanımlıyoruz
 a = 1
 b = 2
 
-# Bu satırlar sadece dosya doğrudan çalıştırıldığında çalışacak
 if __name__ == "__main__":
-    # print fonksiyonu ile a + b = sonuç yazdırıyoruz
     print("{} + {} = {}".format(a, b, add(a, b)))
+
 
