123f32c9e23d6bc1dbd1f98cf9a46eaf3569adc9
Tuncay2006 <zenixthedark@gmail.com>
2025-06-17 14:54:22 +0000

    thrsrea

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 # htt
-def add(a, b):
-    return a + b
-
-a = 1
-b = 2
-
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
-
+    from add_0 import add
+    a=1
+    b=2
+    print("{:d} + {:d} = {:d}" .format (a,b,add(a,b)))
