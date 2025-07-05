ef125158f1c89c9d184e71cf4636aa0dba6f20dd
Tuncay2006 <zenixthedark@gmail.com>
2025-07-05 01:44:47 +0000

    dfg

---
+++ SQL_introduction/0-list_databases.sql
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Prompt for MySQL username
+read -p "Enter MySQL username: " MYSQL_USER
+
+# Prompt for MySQL password securely
+read -s -p "Enter MySQL password: " MYSQL_PASS
+echo
+
+# List all databases
+mysql -u "$MYSQL_USER" -p"$MYSQL_PASS" -e "SHOW DATABASES;"
