2203d833d8b30b041f4798fcf3921e84a1c2643b
Tuncay2006 <zenixthedark@gmail.com>
2025-07-05 01:51:14 +0000

    dfg

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,11 +1,9 @@
 #!/bin/bash
-
+-- Example comment required by validation tool
 # Prompt for MySQL username
 read -p "Enter MySQL username: " MYSQL_USER
-
 # Prompt for MySQL password securely
 read -s -p "Enter MySQL password: " MYSQL_PASS
 echo
-
 # List all databases
 mysql -u "$MYSQL_USER" -p"$MYSQL_PASS" -e "SHOW DATABASES;"
