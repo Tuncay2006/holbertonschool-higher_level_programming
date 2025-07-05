08a97fb14677ad27fe77a9672c40d0179c8f4908
Tuncay2006 <zenixthedark@gmail.com>
2025-07-05 16:14:37 +0000

    drt

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
- -- huhlhhuhhuhu
-mysql -uroot -p <<EOF
+-- Script to show privileges for user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-EOF
