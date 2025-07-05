9f7b0bf7d7766d78478d55d928300fbbc06741a3
Tuncay2006 <zenixthedark@gmail.com>
2025-07-05 16:02:25 +0000

    drt

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-# MySQL root kullanıcısıyla bağlanıp ayrıcalıkları listeleyen script
-# root şifresi sorulmadan çalışması için ~/.my.cnf dosyasına root şifresi yazılmalı
-mysql -u root -e "SHOW GRANTS FOR 'user_0d_1'@'localhost';"
-mysql -u root -e "SHOW GRANTS FOR 'user_0d_2'@'localhost';"
+mysql -uroot -p <<EOF
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+EOF
