5653187ef486d3b99f87d1f245610d6705d99d7f
Tuncay2006 <zenixthedark@gmail.com>
2025-07-05 16:20:36 +0000

    drt

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,3 @@
 -- Script to create user_0d_1 with full privileges
--- 1. Create the user if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- 2. Grant all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
--- 3. Apply the changes
-FLUSH PRIVILEGES;
