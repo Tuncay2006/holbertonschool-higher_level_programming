4813ac240e758322ee66b77a77d1f8af90684e6f
Tuncay2006 <zenixthedark@gmail.com>
2025-07-05 16:26:53 +0000

    drt

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- Script to create user_0d_1 with full privileges
+-- Script to create user_0d_1 with full privileges on the MySQL server
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
