dfb740888ef1f83687f8d7d8122d416b064e9f37
Tuncay2006 <zenixthedark@gmail.com>
2025-07-07 14:33:05 +0000

    gah

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,10 @@
--- Script to create user_0d_1 with full privileges on the MySQL server
+-- 1-create_user.sql
 
+-- Kullanıcıyı oluştur, varsa hata verme
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- Tüm veritabanları ve tablolar üzerinde tüm ayrıcalıkları ver
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Ayrıcalık değişikliklerini uygula
+FLUSH PRIVILEGES;
