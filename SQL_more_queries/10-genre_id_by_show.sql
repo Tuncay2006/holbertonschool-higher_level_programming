e8d0ee092b9ddd5b5e2d0cb29d76aa971ed63379
Tuncay2006 <zenixthedark@gmail.com>
2025-07-07 15:07:02 +0000

    gah

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 --something to explane
-mysql -u root -p hbtn_0d_tvshows < hbtn_0d_tvshows.sql>
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
