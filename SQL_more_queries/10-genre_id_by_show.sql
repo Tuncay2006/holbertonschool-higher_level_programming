11cd8c7ffec78485b33b90ae70a2aeb01af4b447
Tuncay2006 <zenixthedark@gmail.com>
2025-07-07 14:54:52 +0000

    gah

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,6 @@
---something to explane 
+--something to explane
+mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;"
+mysql -u root -p hbtn_0d_tvshows < hbtn_0d_tvshows.sql 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
