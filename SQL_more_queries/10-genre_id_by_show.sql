82006ceb86673b14aa99593909f381d7a7e97f27
Tuncay2006 <zenixthedark@gmail.com>
2025-07-07 14:48:43 +0000

    gah

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
---something to explane Correct output: after creation of multiple data
-CREATE DATABASE hbtn_0d_tvshows;
+--something to explane 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
