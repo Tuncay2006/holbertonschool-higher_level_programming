80e8043bea708beeddd53419e5582bb2b5a1b184
Tuncay2006 <zenixthedark@gmail.com>
2025-07-16 17:11:27 +0000

    gd

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+i#!/usr/bin/python3
+"""Fetches your GitHub user ID using Basic Auth with personal access token"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, token))
+    if response.status_code == 200:
+        json_data = response.json()
+        print(json_data.get("id"))
+    else:
+        print("None")
