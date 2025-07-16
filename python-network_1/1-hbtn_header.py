3340042d0320199f4924ad76925cf0efadc831f2
Tuncay2006 <zenixthedark@gmail.com>
2025-07-16 16:44:46 +0000

    gd

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,10 +4,9 @@
 import urllib.request
 import sys
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
 
-req = urllib.request.Request(url)
-
-with urllib.request.urlopen(req) as response:
-    headers = response.headers
-    print(headers.get("X-Request-Id"))
+    with urllib.request.urlopen(req) as response:
+        print(response.headers.get("X-Request-Id"))
