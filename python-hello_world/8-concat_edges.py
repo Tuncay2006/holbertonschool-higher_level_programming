420ca11c0efca533999548c2dd4963374f10f835
Tuncay2006 <zenixthedark@gmail.com>
2025-07-19 21:40:56 +0000

    fvw

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[6:7] + str[106:112]
+str = str[39:66] + str[66:67] + str[79:83] + str[83:84] + str[0:6]
 print(str)
