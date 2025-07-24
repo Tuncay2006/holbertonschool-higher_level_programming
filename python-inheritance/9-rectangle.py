5f452a6b06d945e79d135002ec2098e0be4a36d7
Tuncay2006 <zenixthedark@gmail.com>
2025-07-24 14:06:27 +0000

    hmx

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,6 +2,7 @@
 """Rectangle that inherits from BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """A class to represent a rectangle using BaseGeometry"""
 
