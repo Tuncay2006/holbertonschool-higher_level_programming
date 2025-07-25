4ca2fbc3aa9b8b2b5d5ccf9836ad7206abe2dd39
Tuncay2006 <zenixthedark@gmail.com>
2025-07-25 21:42:46 +0000

    we

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-print(__import__('this').s[39:67]
-      + __import__('this').s[107:112]
-      + __import__('this').s[:6])
+str = "object-oriented programming with Python"
+print(str[0:39])
